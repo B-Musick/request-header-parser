9e3a816ea1192bffe0fcb54cfb1d7c567e2cff41
Glitch (tourmaline-headstand) <none>
2019-08-17 16:51:26 +0000

    👍📇 Checkpoint ./server.js:6074723/246

--- server.js
+++ server.js
@@ -21,11 +21,14 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  
   res.json({greeting: 'hello API'});
 });
 
 app.get('/api/whoami',(req,res)=>{
-  res.json({"ipaddress":req.ip})
+  let language = req.headers['accept-language'];
+  let userAgent = req.headers['user-agent'];
+  res.json({"ipaddress":req.ip,'language':language,"software":userAgent})
 })
 
 
