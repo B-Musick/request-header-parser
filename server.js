a18a6ce8d0106e826105f1426f3810dc6961a8ca
Glitch (tourmaline-headstand) <none>
2020-05-05 17:44:21 -0500

    Project works and done

--- server.js
+++ server.js
@@ -10,21 +10,16 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-require('dotenv').config;
+// require('dotenv').config;
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
+app.get("/", (req, res)=> { res.sendFile(__dirname + '/views/index.html'); });
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/hello", (req, res)=> { res.json({greeting: 'hello API'}); });
 
 app.get('/api/whoami',(req,res)=>{
   let language = req.headers['accept-language'];
@@ -33,6 +28,6 @@ app.get('/api/whoami',(req,res)=>{
 })
 
 // listen for requests process.env.PORT
-var listener = app.listen(3000, function () {
+var listener = app.listen(3000, ()=> {
   console.log('Your app is listening on port ' + listener.address().port);
 });
